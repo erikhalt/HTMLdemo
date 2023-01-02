c901724927a62fc0d1f72905173d3b170469f16a
Erik Halt <erikhalt@hotmail.com>
2023-01-02 12:20:58 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var SecretMSG = document.getElementById('secretmsg')
+var buttoN = document.getElementById('button')
+
+buttoN.addEventListener("click", ()=>
+    SecretMSG.style.display = "block"
+)
+
